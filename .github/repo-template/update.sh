bbd8c7bc0733edb8bf7d15626cac6615b3e68ddc
Thijs Putman <thijs@studyportals.com>
2020-01-06 17:07:37 +0100

    Add "auto-update" shell-script
    Proof-of-concept at the moment. This should make it possible for
    repositories that use this repo-template to automatically get updated
    with any changes made to the repo-template after they are created.

---
+++ .github/repo-template/update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+git remote add repo-template-php git@github.com:studyportals/repo-template-php.git
+git fetch --quiet repo-template-php
+
+currentRevision=$(git rev-parse --remotes=repo-template-php)
+
+if [ -f .github/repo-template/revision ]; then
+   usedRevision=$(cat .github/repo-template/revision)
+
+  if [ "$currentRevision" != "$usedRevision" ]; then
+    git stash --quiet --include-untracked
+    git diff "$usedRevision"..repo-template-php/master | git apply
+    git add -- *.xml
+    git commit -e -m "Merge branch 'repo-template-php/master' into Woei" -m "Repository was at $usedRevision now at $currentRevision"
+    git stash pop --quiet
+  fi
+
+else
+   echo "$currentRevision" > .github/repo-template/revision
+fi
+
+git remote remove repo-template-php
