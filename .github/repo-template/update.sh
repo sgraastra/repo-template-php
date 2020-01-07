b101832809217aafd4e5a6cd0b1f4abb40072482
Thijs Putman <thijs@studyportals.com>
2020-01-07 17:41:01 +0100

    Fine-tune the update shell-script
    Instead of using "git rev-parse", use "git show-ref" (which is the
    proper way of getting the commit-hash for an active branch). When
    reading the "revision"-file, whitespace are now discarded; the file is
    now also (correctly) added to the commit to keep track of the current
    merge-state. Finally, format a proper commit message.

--- .github/repo-template/update.sh
+++ .github/repo-template/update.sh
@@ -3,16 +3,24 @@
 git remote add repo-template-php git@github.com:studyportals/repo-template-php.git
 git fetch --quiet repo-template-php
 
-currentRevision=$(git rev-parse --remotes=repo-template-php)
+currentRevision=$(git show-ref --hash repo-template-php/master)
 
 if [ -f .github/repo-template/revision ]; then
-   usedRevision=$(cat .github/repo-template/revision)
+  usedRevision=$(tr -d '[:space:]' < .github/repo-template/revision)
 
   if [ "$currentRevision" != "$usedRevision" ]; then
     git stash --quiet --include-untracked
-    git diff "$usedRevision"..repo-template-php/master | git apply
+
+    git diff "$usedRevision"..repo-template-php/master -- . ':(exclude)README.md' | git apply
+    echo "$currentRevision" > .github/repo-template/revision
+
+    commitTitle="Merge branch 'repo-template-php/master' into $(git rev-parse --abbrev-ref HEAD)"
+    commitMessage="Was at studyportals/repo-template-php@$usedRevision"
+    commitMessage="$(printf "%s\n%s" "$commitMessage," "now at studyportals/repo-template-php@$currentRevision.")"
+
     git add .
-    git commit -e -m "Merge branch 'repo-template-php/master' into Woei" -m "Repository was at $usedRevision now at $currentRevision"
+    git commit -e -m "$commitTitle" -m "$commitMessage"
+
     git stash pop --quiet
   fi
 
